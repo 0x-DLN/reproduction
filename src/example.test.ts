507aa61d19f217149a5883607d4d9459c8b7de1c
0xDLN <66573865+0x-DLN@users.noreply.github.com>
2025-02-18 10:52:04 +0100

    Cleanup, removed redundant test

--- src/example.test.ts
+++ src/example.test.ts
@@ -86,27 +86,13 @@ test("#1. Creating an M:1 with underlying M:1 and another ID tries to insert a n
 
 test("#2. 'Nested' M:1 relations created without ID causes their composite to be null", async () => {
   // These have to be created without an ID directly. If created with an ID the test passes.
-  const a = orm.em.create(A, { id: 1, number: 1 });
-  const b = orm.em.create(B, { id: 2, number: 2 });
+  const a = orm.em.create(A, { number: 1 });
+  const b = orm.em.create(B, { number: 2 });
 
   const composite = orm.em.create(Composite, { entityA: a, entityB: b }); // Create a M:1 composite of a and b
 
   // Issue #2: When we try to create a M:1 from composite without the extra PK field, composite appears to be null and we cannot read `entityA`
   orm.em.create(Dependent2, { composite });
-  await orm.em.flush();
-  orm.em.clear();
-});
-
-
-test("#3. 'Nested' M:1 relations created with an id causes foreign key constraints, ", async () => {
-  // These have to be created without an ID directly. If created with an ID the test passes.
-  const a = orm.em.create(A, { id: 1, number: 1 });
-  const b = orm.em.create(B, { id: 2, number: 2 });
-
-  // Issue #3: When we try to create a M:1 from A and B with ID's, the flush causes a FK constraint.
-  // Possibly entities are being created in the wrong order? Not 100% sure
-  orm.em.create(Composite, { entityA: a, entityB: b }); // Create a M:1 composite of a and b
-
   await orm.em.flush();
   orm.em.clear();
 });
