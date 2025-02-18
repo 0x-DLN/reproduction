3566b10367a2e59dc3c1d38dd0485e1eca5c8b11
0xDLN <66573865+0x-DLN@users.noreply.github.com>
2025-02-18 10:36:13 +0100

    Some issues with nested M:1 relations

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,31 +1,67 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import {
+  Entity,
+  ManyToOne,
+  MikroORM,
+  PrimaryKey,
+  PrimaryKeyProp,
+  Property
+} from "@mikro-orm/sqlite";
 
 @Entity()
-class User {
+export class A {
+  @PrimaryKey({ type: "integer", autoincrement: true })
+  public id!: number;
+
+  @Property({ type: "integer" })
+  number!: number;
+}
+
+@Entity()
+export class B {
+  @PrimaryKey({ type: "integer", autoincrement: true })
+  public id!: number;
+
+  @Property({ type: "integer" })
+  number!: number;
+}
+
+@Entity()
+export class Composite {
+  @ManyToOne(() => A, { fieldName: "a_id", primary: true })
+  entityA!: A;
+
+  @ManyToOne(() => B, { fieldName: "b_id", primary: true })
+  entityB!: B;
+
+  [PrimaryKeyProp]?: ["a_id", "b_id"];
+}
+
+@Entity()
+export class Dependent {
+  @ManyToOne(() => Composite, { fieldNames: ["a_id", "b_id"], primary: true })
+  composite!: Composite;
 
   @PrimaryKey()
-  id!: number;
+  anotherId!: number;
 
-  @Property()
-  name: string;
+  [PrimaryKeyProp]?: ["composite", "anotherId"];
+}
 
-  @Property({ unique: true })
-  email: string;
+@Entity()
+export class Dependent2 {
+  @ManyToOne(() => Composite, { fieldNames: ["a_id", "b_id"], primary: true })
+  composite!: Composite;
 
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
-  }
-
+  [PrimaryKeyProp]?: ["composite"];
 }
 
 let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
-    dbName: ':memory:',
-    entities: [User],
-    debug: ['query', 'query-params'],
+    dbName: ":memory:",
+    entities: [A, B, Composite, Dependent, Dependent2],
+    debug: ["query", "query-params"],
     allowGlobalContext: true, // only for testing
   });
   await orm.schema.refreshDatabase();
@@ -35,17 +71,42 @@ afterAll(async () => {
   await orm.close(true);
 });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
+test("#1. Creating an M:1 with underlying M:1 and another ID tries to insert a null value into underlying M:1", async () => {
+  // These have to be created without an ID directly. If created with an ID the test passes.
+  const a = orm.em.create(A, { number: 1 });
+  const b = orm.em.create(B, { number: 2 });
+
+  const composite = orm.em.create(Composite, { entityA: a, entityB: b }); // Create a M:1 composite of a and b
+
+  // Issue #1: When we try to create a M:1 from composite with an extra PK field, `a_id` is filled, while `b_id` is null
+  orm.em.create(Dependent, { composite, anotherId: 3 });
   await orm.em.flush();
   orm.em.clear();
+});
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
+test("#2. 'Nested' M:1 relations created without ID causes their composite to be null", async () => {
+  // These have to be created without an ID directly. If created with an ID the test passes.
+  const a = orm.em.create(A, { id: 1, number: 1 });
+  const b = orm.em.create(B, { id: 2, number: 2 });
+
+  const composite = orm.em.create(Composite, { entityA: a, entityB: b }); // Create a M:1 composite of a and b
+
+  // Issue #2: When we try to create a M:1 from composite without the extra PK field, composite appears to be null and we cannot read `entityA`
+  orm.em.create(Dependent2, { composite });
   await orm.em.flush();
+  orm.em.clear();
+});
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+
+test("#3. 'Nested' M:1 relations created with an id causes foreign key constraints, ", async () => {
+  // These have to be created without an ID directly. If created with an ID the test passes.
+  const a = orm.em.create(A, { id: 1, number: 1 });
+  const b = orm.em.create(B, { id: 2, number: 2 });
+
+  // Issue #3: When we try to create a M:1 from A and B with ID's, the flush causes a FK constraint.
+  // Possibly entities are being created in the wrong order? Not 100% sure
+  orm.em.create(Composite, { entityA: a, entityB: b }); // Create a M:1 composite of a and b
+
+  await orm.em.flush();
+  orm.em.clear();
 });
